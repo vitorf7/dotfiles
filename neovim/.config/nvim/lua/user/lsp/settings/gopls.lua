fd6a340922e9ad84834e35866d94bb523b2576dc
Vitor Faiante <vitor@paddle.com>
2022-08-30 17:29:47 +0100

    first commit

---
+++ neovim/.config/nvim/lua/user/lsp/settings/gopls.lua
@@ -0,0 +1,8 @@
+return {
+  codelenses = {
+    generate = true, -- show the `go generate` lens.
+    gc_details = true, --  // Show a code lens toggling the display of gc's choices.
+    test = true,
+    tidy = true,
+  },
+}
---
+++ zsh/.zprofile
@@ -0,0 +1,5 @@
+export GITHUB_ACCESS_TOKEN=$(security find-generic-password -a "pdl" -s "paddle_github_access_token" -w)
+
+
+# Added by Toolbox App
+export PATH="$PATH:/usr/local/bin"
