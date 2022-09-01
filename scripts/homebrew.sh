5a525d2cfac6838a0482ad8429f287e8c5c054c7
Vitor Faiante <vitor@paddle.com>
2022-09-01 13:08:36 +0100

    Fix homebrew

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -2,9 +2,13 @@ echo "Installing homebrew ..."
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo "Add Homebrew to Path"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 echo "Copying Brewfile"
 
-cp ../homebrew/Brewfile $HOME/.Brewfile
+cp homebrew/Brewfile $HOME/.Brewfile
 
 echo "Install All homebrew dependencies using brew bundle"
 
