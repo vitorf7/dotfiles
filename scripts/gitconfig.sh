087457cee947f0eca97e6b96783fe6646563de13
Vitor Faiante <vitorf7@gmail.com>
2022-09-26 15:56:47 +0100

    Add personal gi config to shell script

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -8,6 +8,13 @@ if [[ $usePrivateModules == "Y" ]]; then
   read -p "Please enter a GitHub access token to be used: " ghToken
   read -p "What is the github organisation root name: " ghOrg
 fi
+read -p "Do you want to add a personal git configuration (Y/N): " addPersonalConfig
+if [[ $addPersonalConfig == "Y" ]]; then
+  read -p "Please enter the personal git name to be used: " personalGitName
+  read -p "Please enter the personal git email to be used: " personalGitEmail
+  read -p "Which personal signing key do you want to use: " personalSigningKey
+  read -p "What is the personal work root code location: " personalCodeLocation
+fi
 
 
 echo "The GitHub access token you enter was $ghToken"
@@ -37,5 +44,24 @@ if [[ $usePrivateModules == "Y" ]]; then
 cat <<EOF >> $HOME/.gitconfig
 [url "https://$ghToken:@github.com/$ghOrg"]
   insteadOf = https://github.com/$ghOrg
+EOF
+fi
+
+if [[ $addPersonalConfig == "Y" ]]; then
+cat <<EOF >> $HOME/.gitconfig
+[includeIf "gitdir:$personalCodeLocation"]
+  path = ~/.gitconfig.personal
+[includeIf "gitdir:~/configfiles/"]
+  path = ~/.gitconfig.personal
+EOF
+
+mkdir -p $personalCodeLocation
+
+
+cat <<EOF >> $HOME/.gitconfig.personal
+[user]
+  email = $personalGitEmail
+  name = $personalGitName
+  signingKey = $personalSigningKey
 EOF
 fi
