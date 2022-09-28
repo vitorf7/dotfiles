d7bb60b7c87e104b4e5c8e1b657f7ef56833b63e
Vitor Faiante <vitorf7@gmail.com>
2022-09-28 13:46:12 +0100

    Add git submodule for nvim config

--- neovim/.config/nvim/lua/user/lsp/settings/gopls.lua
+++
@@ -1,8 +0,0 @@
-return {
-  codelenses = {
-    generate = true, -- show the `go generate` lens.
-    gc_details = true, --  // Show a code lens toggling the display of gc's choices.
-    test = true,
-    tidy = true,
-  },
-}
--- neovim/.config/nvim/lua/user/lsp/settings/phpactor.lua
+++
@@ -1,5 +0,0 @@
-local phpactor_capabilities = vim.lsp.protocol.make_client_capabilities()
-phpactor_capabilities['textDocument']['codeAction'] = {}
-return {
-     capabilities = phpactor_capabilities,
-}
