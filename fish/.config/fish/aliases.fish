0419c28ae8d1d823e08b39250d531ad8c0ca62af
Vitor Faiante <vitorf7@gmail.com>
2023-10-18 19:06:57 +0100

    feat: add fish and starship config

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,71 @@
+# Git
+alias ga="git add"
+alias gaa="git add ."
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gcmps="gitCommitWithMessageAndPush"
+alias gf="git fetch"
+alias gpl="git pull"
+alias gps="git push"
+alias gst="git status"
+alias gs="git switch"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gdf="git diff"
+alias gch="git checkout"
+
+#PhpSpec
+alias specr="phpspec run"
+alias specrcat="phpspec run --format nyan.cat"
+alias specrdino="phpspec run --format nyan.dino"
+alias specrcrab="phpspec run --format nyan.crab"
+alias specd="phpspec describe "
+
+# Docker helpers
+alias docker-stop-everything='docker stop $(docker ps -a -q)'
+alias docker-killall='docker-stop-everything && docker rm $(docker ps -a -q)'
+alias docker-prune='docker system prune -a --volumes -f'
+alias docker-fresh='docker-stop-everything && docker-prune'
+
+function docker-fatality
+    echo '============      FINISH IT   ============'
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    docker system prune -a --volumes -f
+    echo '============      FATALITY    ============'
+    echo '============ FLAWLESS VICTORY ============'
+end
+
+alias brew-up-to-date="brew update && brew upgrade"
+alias brew-force-update-casks="brew update && brew upgrade --cask --greedy --force"
+alias brew-upgrade-everything="brew update && brew upgrade && brew upgrade --cask --greedy --force"
+
+# Exa alias for ls (https://github.com/ogham/exa)
+alias lh="eza --icons -lha"
+alias ls="eza --icons"
+
+# Mac setup for pomo
+alias work="timer 25m && terminal-notifier -message 'Pomodoro'\
+        -title 'Work Timer is up! Take a Break 😊'\
+        -appIcon '~/Pictures/pumpkin.png'\
+        -sound Crystal"
+
+alias pairing-break="timer 5m && terminal-notifier -message 'Pomodoro'\
+        -title 'Break is over! Get back to work 😬'\
+        -appIcon '~/Pictures/pumpkin.png'\
+        -sound Crystal"
+
+alias rest="timer 30m && terminal-notifier -message 'Pomodoro'\
+        -title 'Break is over! Get back to work 😬'\
+        -appIcon '~/Pictures/pumpkin.png'\
+        -sound Crystal"
+
+# cat
+alias cat="bat"
+
+# Ngrok
+alias ngrok="ngrok --authtoken=$NGROK_AUTHTOKEN"
+
+# GH GLI
+# unset GITHUB_TOKEN from gh's process environment and run gh command.
+# see https://stackoverflow.com/a/41749660 & https://github.com/cli/cli/issues/3799 for more.
+alias gh="env -u GITHUB_TOKEN gh $1"
