57d1af794b72d17d775e521f48f59a4feed24422
Vitor Faiante <vitorf7@gmail.com>
2024-02-01 15:27:35 +0000

    feat: update fzf and add config to use sesh with fzf

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,4 +1,4 @@
-set -Ux FZF_DEFAULT_OPTS "--height=80% --exact --reverse --border rounded --inline-info --prompt='🔭 ' --pointer=' ' --marker=' ' --ansi --color='16,bg+:-1,gutter:-1,prompt:5,pointer:5,marker:6,border:4,label:4,header:italic' --preview 'file --mime-type {} | bat --color=always {}' --preview-window='right,60%:hidden' --bind \?:toggle-preview --bind pgup:preview-page-up --bind pgdn:preview-page-down"
+set -Ux FZF_DEFAULT_OPTS "--height=80% --exact --reverse --border rounded --inline-info --prompt='🔭 ' --pointer='👉' --marker=' ' --ansi --color='16,bg+:-1,gutter:-1,prompt:5,pointer:5,marker:6,border:4,label:4,header:italic' --preview 'file --mime-type {} | bat --color=always {}' --preview-window='right,60%:hidden' --bind \?:toggle-preview --bind pgup:preview-page-up --bind pgdn:preview-page-down"
 # set -Ux FZF_DEFAULT_OPTS "--exact --reverse --border rounded --inline-info --ansi --pointer='' --marker='' --color='16,bg+:-1,gutter:-1,prompt:5,pointer:5,marker:6,border:4,label:4,header:italic'"
 
 # --pointer=' ' \
