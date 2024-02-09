726f11c3d2d1d5409c3eecb8968927ff34873020
Vitor Faiante <vitorf7@gmail.com>
2024-02-09 16:12:30 +0000

    feat: add gitconfig alias

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -5,21 +5,20 @@ read -p "Please enter the git email to be used: " gitEmail
 read -p "Which signing key do you want to use: " signingKey
 read -p "Do you want to be able to download private modules (Y/N): " usePrivateModules
 if [[ $usePrivateModules == "Y" ]]; then
-  read -p "Please enter a GitHub access token to be used: " ghToken
-  read -p "What is the github organisation root name: " ghOrg
+	read -p "Please enter a GitHub access token to be used: " ghToken
+	read -p "What is the github organisation root name: " ghOrg
 fi
 read -p "Do you want to add a personal git configuration (Y/N): " addPersonalConfig
 if [[ $addPersonalConfig == "Y" ]]; then
-  read -p "Please enter the personal git name to be used: " personalGitName
-  read -p "Please enter the personal git email to be used: " personalGitEmail
-  read -p "Which personal signing key do you want to use: " personalSigningKey
-  read -p "What is the personal work root code location: " personalCodeLocation
+	read -p "Please enter the personal git name to be used: " personalGitName
+	read -p "Please enter the personal git email to be used: " personalGitEmail
+	read -p "Which personal signing key do you want to use: " personalSigningKey
+	read -p "What is the personal work root code location: " personalCodeLocation
 fi
-
 
 echo "The GitHub access token you enter was $ghToken"
 
-cat <<EOF > $HOME/.gitconfig
+cat <<EOF >$HOME/.gitconfig
 [user]
   email = $gitEmail
   name = $gitName
@@ -37,28 +36,29 @@ cat <<EOF > $HOME/.gitconfig
   program = /Applications/1Password.app/Contents/MacOS/op-ssh-sign
 [commit]
   gpgsign = true
+[alias]
+  leaderboard = shortlog --summary --numbered --all --no-merges
+  sbr = "!rm ${GIT_PREFIX}$1 && git checkout -- ${GIT_PREFIX}$1 #"
 EOF
 
-
 if [[ $usePrivateModules == "Y" ]]; then
-cat <<EOF >> $HOME/.gitconfig
+	cat <<EOF >>$HOME/.gitconfig
 [url "https://$ghToken:@github.com/$ghOrg"]
   insteadOf = https://github.com/$ghOrg
 EOF
 fi
 
 if [[ $addPersonalConfig == "Y" ]]; then
-cat <<EOF >> $HOME/.gitconfig
+	cat <<EOF >>$HOME/.gitconfig
 [includeIf "gitdir:$personalCodeLocation"]
   path = ~/.gitconfig.personal
 [includeIf "gitdir:~/configfiles/"]
   path = ~/.gitconfig.personal
 EOF
 
-mkdir -p $personalCodeLocation
+	mkdir -p $personalCodeLocation
 
-
-cat <<EOF >> $HOME/.gitconfig.personal
+	cat <<EOF >>$HOME/.gitconfig.personal
 [user]
   email = $personalGitEmail
   name = $personalGitName
