6f0bcf05bd87e7119e0e0fe65b6dac61d63c8c18
Vitor Faiante <vitorf7@gmail.com>
2026-06-24 19:48:57 +0100

    fix: add gnome back in

--- nixos/.nixos/modules/system/display.nix
+++ nixos/.nixos/modules/system/display.nix
@@ -3,5 +3,6 @@
 lib.mkIf config.vitorf7.desktop.hyprland.enable {
   services.xserver.enable = true;
   services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   programs.hyprland.enable = true;
 }
