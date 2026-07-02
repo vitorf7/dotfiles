3b9d683e6e1cfd558d1d77b714db11dbf6e53c58
Vitor Faiante <vitorf7@gmail.com>
2026-07-02 19:16:15 +0100

    feat: add gtk and hypr cursor theme

--- nixos/.nixos/modules/home/theming.nix
+++ nixos/.nixos/modules/home/theming.nix
@@ -3,13 +3,23 @@
 lib.mkIf osConfig.vitorf7.desktop.enable {
   home.packages = with pkgs; [
     papirus-icon-theme
-    # Add your preferred GTK theme package here, e.g.:
-    # rose-pine-gtk-theme
-    # catppuccin-gtk
+    rose-pine-gtk-theme
+    rose-pine-hyprcursor
   ];
+
+  home.pointerCursor = {
+    name = "rose-pine-hyprcursor";
+    package = pkgs.rose-pine-hyprcursor;
+    size = 24;
+    gtk.enable = true;
+  };
 
   gtk = {
     enable = true;
+    theme = {
+      name = "Rose-Pine";
+      package = pkgs.rose-pine-gtk-theme;
+    };
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
