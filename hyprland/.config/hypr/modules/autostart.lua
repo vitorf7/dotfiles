ce0968c556a3f0837f1b6c69b697d9f7f476ec8c
Vitor Faiante <vitorf7@gmail.com>
2026-07-04 19:08:34 +0100

    refactor: do not turn on waybar at start

--- hyprland/.config/hypr/modules/autostart.lua
+++ hyprland/.config/hypr/modules/autostart.lua
@@ -2,7 +2,7 @@
 hl.on("hyprland.start", function()
     hl.exec_cmd("hypridle")
     hl.exec_cmd("awww-daemon")
-    hl.exec_cmd("waybar")
+    -- hl.exec_cmd("waybar")
     hl.exec_cmd("swaync")
     hl.exec_cmd("vicinae server")
     hl.exec_cmd("avizo-service")
