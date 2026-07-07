6ccbd44ec00a0af1b5fc879374d75d6fda0bd916
Vitor Faiante <vitorf7@gmail.com>
2026-07-07 14:07:51 +0100

    feat: mouseless now available as a flatpak instead

--- nixos/.nixos/hosts/thinkpad-t480/configuration.nix
+++ nixos/.nixos/hosts/thinkpad-t480/configuration.nix
@@ -27,6 +27,7 @@
   vitorf7.desktop.quickshell.enable = true;
   vitorf7.desktop.qs_brain_shell.enable = false;
   vitorf7.desktop.ambxst.enable = true;
+  vitorf7.desktop.flatpak.enable = true;
   vitorf7.hardware.fingerprint.enable = true;
   vitorf7.hardware.nvidia.enable = true;
 
