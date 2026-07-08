292a96bd4f8400d7a3a91d2cb9a59583aea593a1
Vitor Faiante <vitorf7@gmail.com>
2026-07-08 01:07:14 +0100

    feat: add mousless via flatpak

--- nixos/.nixos/modules/system/users.nix
+++ nixos/.nixos/modules/system/users.nix
@@ -5,7 +5,12 @@
 
   users.users.vitorf7 = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" "input" ];
     shell = pkgs.fish;
   };
+
+  # Allow the input group to write to /dev/uinput (required by Mouseless)
+  services.udev.extraRules = ''
+    KERNEL=="uinput", GROUP="input", MODE="0660"
+  '';
 }
