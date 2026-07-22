02a98c80dd4174eaed0ef1c163fe44902fd249bf
Vitor Faiante <vitorf7@gmail.com>
2026-07-22 23:24:10 +0100

    refactor: change monitors config

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -1,24 +1,8 @@
--- This is an example Hyprland config file.
-
--- Refer to the wiki for more information.
-
--- https://wiki.hyprland.org/Configuring/
-
--- Please note not all available settings / options are set here.
-
--- For a full list, see the wiki
-
--- You can split this configuration into multiple files
-
--- Create your files separately and then link them to this file like this:
-
--- source = ~/.config/hypr/myColors.conf
-
 -- Source all modules
-
--- source = $HOME/.config/hypr/modules/variables.conf
 require("modules.environment")
 require("modules.monitors")
+require("modules.monitor_manager")
+require("modules.clamshell")
 require("modules.autostart")
 require("modules.input")
 require("modules.layouts")
@@ -26,7 +10,6 @@ require("modules.appearance")
 require("modules.animations")
 require("modules.keybindings")
 require("modules.windowrules")
-require("modules.clamshell")
 
 -- HyprMod managed settings
 require("hyprland-gui")
