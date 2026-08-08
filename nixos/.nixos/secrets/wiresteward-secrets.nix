6dffcc8cf8038ed93c315f4767309a0f021c22de
Vitor Faiante <vitorf7@gmail.com>
2026-08-08 03:49:49 +0100

    refactor: change config

--- nixos/.nixos/secrets/wiresteward-secrets.nix
+++ nixos/.nixos/secrets/wiresteward-secrets.nix
@@ -1,4 +1,4 @@
-{
-  uwDnsServer = "10.253.253.253";
-  uwDnsDomains = "~telecomplus.internal ~tp.private";
-}
+# STRONGBOX ENCRYPTED RESOURCE ; See https://github.com/uw-labs/strongbox
+tp821IbPFJga2iTEIcNyKjWCiyoOlaVW+pOwHukjb7/oJUOKPfV6+1L1aaH0B4cjq4issuy/WGZq
+QwSBwtVf2NjeSTTjEpXvH9kwJ1yQXMFyfpFbRa+3rOyHJFbVtypeQJT/dZTSOqPSH+ndlIGuVPCw
+OtX1DA==
