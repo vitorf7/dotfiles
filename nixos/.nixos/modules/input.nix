a5ff8c3724093e6891e1248b28ddc83db0692a69
Vitor Faiante <vitorf7@gmail.com>
2026-08-26 17:34:02 +0100

    feat: add openlogi

--- nixos/.nixos/modules/input.nix
+++ nixos/.nixos/modules/input.nix
@@ -6,10 +6,13 @@
       "homerow"
       "keycastr"
       "mouseless@preview"
+      "openlogi"
     ];
   };
 
   flake.modules.homeManager.input = { config, pkgs, lib, ... }:
+    home.packages = lib.optionals pkgs.stdenv.isLinux [ pkgs.openlogi ];
+
     let
       isDarwin = pkgs.stdenv.isDarwin;
       link = config.lib.file.mkOutOfStoreSymlink;
